e4a4a16c69816fd9bb81ec07fc99bdf3959b84b8
Miodrag Piletic <miodragpiletic@Miodrags-MBP-2.fritz.box>
2025-07-24 18:21:21 +0200

    fix errors

--- scripts/script.js
+++ scripts/script.js
@@ -11,25 +11,48 @@ function enableLoaderLinks() {
   });
 }
 
+function hideLoader() {
+  const loader = document.getElementById("loader");
+  if (loader) {
+    loader.classList.remove("active");
+  }
+}
+
 function loadLayoutParts() {
   fetch("/navbar.html")
     .then((res) => res.text())
     .then((html) => {
-      document.getElementById("navbar-placeholder").innerHTML = html;
+      const navbarElement = document.getElementById("navbar-placeholder");
+      if (navbarElement) {
+        navbarElement.innerHTML = html;
+      }
       enableLoaderLinks();
     });
 
   fetch("/footer.html")
     .then((res) => res.text())
     .then((html) => {
-      document.getElementById("footer-placeholder").innerHTML = html;
+      const footerElement = document.getElementById("footer-placeholder");
+      if (footerElement) {
+        footerElement.innerHTML = html;
+      }
     });
 
   fetch("email/contact_form.html")
     .then((res) => res.text())
     .then((html) => {
-      document.getElementById("contact-form-placeholder").innerHTML = html;
+      const contactFormEl = document.getElementById("contact-form-placeholder");
+      if (contactFormEl) {
+        contactFormEl.innerHTML = html;
+      }
     });
 }
 
-document.addEventListener("DOMContentLoaded", loadLayoutParts);
+window.addEventListener("pageshow", (event) => {
+  document.getElementById("loader").classList.remove("active");
+});
+
+document.addEventListener("DOMContentLoaded", function () {
+  loadLayoutParts();
+  hideLoader();
+});
